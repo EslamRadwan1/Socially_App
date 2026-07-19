9a6cf46be88bf305565191847f4c0e2c8615f194
Eslam Radwan <eslam23279@gmail.com>
2026-07-19 17:09:58 +0300

    Create Login and Logout and Setup UI and Alert

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
+const baseUrl = "https://tarmeezacademy.com/api/v1";
+const navBtns = document.getElementById("buttons");
+let mainLoginBtn;
 const postsArea = document.getElementById("content-area");
+
+setupUI();
+getPosts();
 
 async function getPosts() {
   try {
-    const response = await fetch("https://tarmeezacademy.com/api/v1/posts?limit=50");
-    if (!response.ok) throw new Error("Request Failed");
+    const response = await fetch(`${baseUrl}/posts?limit=50`);
+    if (!response.ok) {
+      showAlert("Request Failed", "bg-red-500/80");
+      throw new Error("Request Failed");
+    }
     const postsResponse = await response.json();
     const posts = postsResponse.data;
-    // showPost(posts[0]);
     for (let post of posts) {
       showPost(post);
     }
@@ -14,8 +22,6 @@ async function getPosts() {
     console.log(err);
   }
 }
-getPosts();
-
 function showPost(postObj) {
   const post = document.createElement("div");
   post.className = "post";
@@ -24,8 +30,13 @@ function showPost(postObj) {
   header.className = "header";
   const userImg = document.createElement("img");
   userImg.className = "header-img";
-  userImg.src = postObj.author.profile_image;
   userImg.alt = "User Image";
+  if (Object.keys(postObj.author.profile_image).length === 0) {
+    userImg.src =
+      "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=";
+  } else {
+    userImg.src = postObj.author.profile_image;
+  }
   const info = document.createElement("div");
   const userNameDiv = document.createElement("div");
   const userName = document.createTextNode(postObj.author.name);
@@ -79,4 +90,157 @@ function showPost(postObj) {
 
   post.append(header, body, footer);
   postsArea.append(post);
+}
+function loginForm() {
+  const form = document.createElement("div");
+  form.className = "form";
+
+  const closeBtn = document.createElement("button");
+  closeBtn.className = "close-btn";
+  const closeIcon = document.createElement("i");
+  closeIcon.classList.add("fa-solid", "fa-xmark");
+  closeBtn.append(closeIcon);
+  closeBtn.addEventListener("click", () => {
+    form.remove();
+    mainLoginBtn.disabled = false;
+  });
+
+  const heading = document.createElement("h1");
+  heading.textContent = "Sign-In";
+
+  const userNameDiv = document.createElement("div");
+  userNameDiv.className = "field";
+  const userNameLabel = document.createElement("label");
+  userNameLabel.textContent = "User Name";
+  const userNameInput = document.createElement("input");
+  userNameInput.type = "text";
+  userNameDiv.append(userNameLabel, userNameInput);
+
+  const passwordDiv = document.createElement("div");
+  passwordDiv.className = "field";
+  const passwordLabel = document.createElement("label");
+  passwordLabel.textContent = "Password";
+  const passwordInput = document.createElement("input");
+  passwordInput.type = "password";
+  passwordDiv.append(passwordLabel, passwordInput);
+
+  const loginBtn = document.createElement("button");
+  loginBtn.textContent = "Login";
+  loginBtn.className = "login-btn";
+  loginBtn.addEventListener("click", () => {
+    const userName = userNameInput.value;
+    const password = passwordInput.value;
+    if (userName === "" || password === "") {
+      showAlert("Please fill in all fields", "bg-orange-500/80");
+      return;
+    }
+    checkUser(userName, password);
+  });
+
+  const createAcount = document.createElement("div");
+  createAcount.className = "create-acount";
+  const createAcountText = document.createElement("p");
+  createAcountText.textContent = "Don't Have Acount?";
+  const createAcountSpan = document.createElement("span");
+  createAcountSpan.textContent = "Create Acount";
+  createAcount.append(createAcountText, createAcountSpan);
+
+  form.append(
+    heading,
+    closeBtn,
+    userNameDiv,
+    passwordDiv,
+    loginBtn,
+    createAcount,
+  );
+  document.body.append(form);
+}
+async function checkUser(userName, password) {
+  try {
+    const response = await fetch(`${baseUrl}/login`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: userName,
+        password: password,
+      }),
+    });
+    if (response.status === 422) {
+      showAlert("Invalid username or password", "bg-red-500/80");
+    }
+    if (!response.ok) throw new Error("Request Failed");
+    const userResponse = await response.json();
+    const user = userResponse.user;
+    const token = userResponse.token;
+    document.querySelector(".form").remove();
+    localStorage.setItem("token", token);
+    localStorage.setItem("user", JSON.stringify(user));
+    setupUI();
+    showAlert("Login Succussfully", "bg-green-500/80");
+  } catch (err) {
+    console.log(err);
+  }
+}
+function setupUI() {
+  const token = localStorage.getItem("token");
+  if (token == null) {
+    navBtns.innerHTML = `
+            <button id="login-btn" class="bg-white px-4 py-1.5 rounded-full border border-sky-500 cursor-pointer hover:bg-sky-500 hover:border-sky-500 hover:text-white transition duration-300 flex items-center gap-0.5 disabled:bg-gray-400 disabled:border-gray-400 disabled:text-gray-600 disabled:cursor-not-allowed">
+              Login
+              <i class="fa-thin fa-right-to-bracket"></i>
+            </button>`;
+    mainLoginBtn = document.getElementById("login-btn");
+    mainLoginBtn.addEventListener("click", () => {
+      loginForm();
+      mainLoginBtn.disabled = true;
+    });
+  } else {
+    const user = localStorage.getItem("user");
+    let profileImage;
+    if (Object.keys(JSON.parse(user).profile_image).length === 0) {
+      profileImage =
+        "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0=";
+    } else {
+      profileImage = JSON.parse(user).profile_image;
+    }
+    navBtns.innerHTML = `
+          <div id="profile">
+            <img
+              class="size-11 rounded-full border-2 border-white"
+              src="${profileImage}"
+              alt="Profile Img"
+            />
+          </div>
+          <button id="logout-btn" class="bg-white text-sm  px-2 py-1 rounded-full border border-red-500 cursor-pointer hover:bg-red-500 hover:border-red-500 hover:text-white transition duration-300 flex items-center gap-0.5 disabled:bg-gray-400 disabled:border-gray-400 disabled:text-gray-600 disabled:cursor-not-allowed">
+              Logout
+              <i class="fa-thin fa-right-to-bracket"></i>
+            </button>`;
+    const logoutBtn = document.getElementById("logout-btn");
+    logoutBtn.addEventListener("click", () => {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      setupUI();
+      showAlert("Logged Out Successfully", "bg-pink-500/80");
+    });
+  }
+}
+function showAlert(message, style) {
+  const div = document.createElement("div");
+  div.classList.add("alert", style);
+  const icon = document.createElement("i");
+  icon.classList.add("fa-duotone", "fa-solid", "fa-circle-info");
+  const msg = document.createElement("p");
+  msg.textContent = message;
+  div.append(icon, msg);
+  document.querySelector("main").appendChild(div);
+
+  setTimeout(() => {
+    div.classList.add("opacity-0");
+    setTimeout(() => {
+      div.remove();
+    }, 500);
+  }, 2000);
 }
